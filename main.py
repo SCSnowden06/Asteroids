b4abf609903be348f4d7c94fe95087fb7477d6de
SCSnowden06 <SCSnowden06@gmail.com>
2025-04-04 19:18:37 -0400

    cleaned up a little more. Im sure my pylint score is abyssmal, but i cant figure out how to run it in venv rn

--- main.py
+++ main.py
@@ -5,17 +5,16 @@ from player import *
 
 def main(): 
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))     # System initializations
     clock = pygame.time.Clock()
     dt = 0
 
-    
-
-    updateable = pygame.sprite.Group()
+    updateable = pygame.sprite.Group()                                  # Group initializations
     drawable = pygame.sprite.Group()
     Player.containers = (drawable, updateable)
 
-    jit = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
+    jit = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))                   # Player initializations
 
     while True:                                                 
         for event in pygame.event.get():                        #lets the exit button actually work
@@ -23,17 +22,14 @@ def main():
                 return
         screen.fill((0,0,0))                                    #here and above are initializing game,   
 
-        updateable.update(dt)                                         #game loop starts here
+        updateable.update(dt)                                   #game loop starts here
         for sprite in drawable:
             sprite.draw(screen)
-        dt = clock.tick(60) / 1000  
-
-        pygame.display.flip()                                   #update screen
+         
+        dt = clock.tick(60) / 1000 
+        pygame.display.flip()                                   # update screen
         
 
     
-
-
-
-if __name__ == "__main__":
+if __name__ == "__main__":                                      # Run
     main()
