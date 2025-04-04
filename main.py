94714eab4b29ffc80b59e28686d301a3d49cfe65
SCSnowden06 <SCSnowden06@gmail.com>
2025-04-03 20:05:48 -0400

    added pygame.clock and used tick method to limit screen update to 60fps

--- main.py
+++ main.py
@@ -5,16 +5,22 @@ from constants import *
 
 def main(): 
     pg.init()
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pg.time.Clock()
+    dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
         screen.fill((0,0,0))
+        dt = clock.tick(60) / 1000  
         pg.display.flip()
+        
 
     
 
